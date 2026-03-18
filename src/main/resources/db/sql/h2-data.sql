06ead6a0bf8d4c0115ad27dc34ed9228f6e569fc
haemin4738 <leeheamin12@gmail.com>
2026-03-18 15:41:38 +0900

    [Feat] h2 초기 데이터, 스키마 수정

--- src/main/resources/db/sql/h2-data.sql
+++ src/main/resources/db/sql/h2-data.sql
@@ -1,3 +1,8 @@
--- TODO data
--- example
-insert into members (name, last_modified_date) values('bob', now());
+-- 기본 계정 삽입
+-- admin / admin1234
+insert into users (username, password, role)
+values ('admin', '$2a$10$N.zmdr9k7uOCQb376NoUnuTJ8iAt6Z5EHsM8lE9lBOsl7iAt6Z5EH', 'ADMIN');
+
+-- user1 / user1234
+insert into users (username, password, role)
+values ('user1', '$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO1S0W.xFwC', 'USER');
--- src/main/resources/db/sql/h2-schema.sql
+++ src/main/resources/db/sql/h2-schema.sql
@@ -1,9 +1,21 @@
 -- TODO schema
 -- example
-create table members
+create table users
+(
+    id       bigint primary key not null auto_increment,
+    username varchar(50)        not null unique,
+    password varchar(255)       not null,
+    role     varchar(20)        not null
+);
+
+create table contents
 (
     id                 bigint primary key      not null auto_increment,
-    name               varchar(50)             not null,
-    created_date       timestamp default now() not null,
-    last_modified_date timestamp
+    title              varchar(100)            not null,
+    description        text,
+    view_count         bigint                  not null default 0,
+    created_date       timestamp,
+    created_by         varchar(50)             not null,
+    last_modified_date timestamp,
+    last_modified_by   varchar(50)
 );
